fa26b6f4655c8e1889dc98248fe618e7b2b6e0f1
Karl Swedberg <kswedberg@gmail.com>
2023-10-04 15:46:35 -0400

    Add tests

---
+++ test/basic.test.ts
@@ -0,0 +1,40 @@
+import {describe, it, expect, afterAll} from 'vitest';
+import {fileURLToPath} from 'node:url';
+import {setup} from '@nuxt/test-utils';
+import {readFileSync, rmSync} from 'node:fs';
+import {join} from 'node:path';
+
+describe('globals', async() => {
+  const rootDir = fileURLToPath(new URL('./fixtures/basic', import.meta.url));
+  const outputDir = join(rootDir, 'gitignore');
+  const outputFile = join(outputDir, '.eslint.globals.cjs');
+
+
+  await setup({
+    rootDir,
+  });
+
+  afterAll(() => {
+    rmSync(outputDir, {force: true, recursive: true});
+  });
+
+
+  it('outputs eslint globals file', () => {
+    const text = readFileSync(outputFile, 'utf-8');
+
+    expect(text).toContain('globals:');
+  });
+
+  it('adds default globals', () => {
+    const text = readFileSync(outputFile, 'utf-8');
+
+    expect(text).toContain('definePageMeta:');
+  });
+
+  it('adds globals from composables directory', () => {
+    const text = readFileSync(outputFile, 'utf-8');
+
+    expect(text).toContain('useTestMe:');
+  });
+
+});
---
+++ test/fixtures/basic/nuxt.config.ts
@@ -0,0 +1,8 @@
+import eslintGlobals from '../../../src/module';
+import {defineNuxtConfig} from 'nuxt/config';
+
+export default defineNuxtConfig({
+  modules: [
+    [eslintGlobals, {outputDir: 'gitignore'}],
+  ],
+});
